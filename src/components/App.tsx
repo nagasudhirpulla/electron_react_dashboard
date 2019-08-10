44975021b1afe89af72e861ca1bdb0f4c91aa1e6
nagasudhirpulla <nagasudhirpulla@gmail.com>
2019-08-11 00:32:14 +0530

    changes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -21,7 +21,7 @@ class App extends React.Component<AppProps, AppState> {
     currentBreakpoint: "lg",
     compactType: "vertical",
     mounted: false,
-    layouts: { lg: generateLayout() }
+    layouts: { lg: this.props.initialLayout }
   };
 
   componentDidMount() {
@@ -52,14 +52,14 @@ class App extends React.Component<AppProps, AppState> {
   onAddItem = () => {
     this.setState({
       layouts: {
-        lg: this.state.layouts.lg.concat({
+        lg: [...this.state.layouts.lg, {
           i: uuid(),
-          x: (this.state.layouts.lg.length * 2) % (this.props.cols[this.state.currentBreakpoint] || 12),
+          x: 0,
           y: Infinity, // puts it at the bottom
           w: 2,
           h: 2,
           static: false
-        })
+        }]
       }
     } as AppState);
   };
@@ -67,16 +67,15 @@ class App extends React.Component<AppProps, AppState> {
   onRemoveItem = (ind: number) => {
     this.setState({
       layouts: {
-        lg: [...this.state.layouts.lg.slice(0, ind), ...this.state.layouts.lg.slice(0, ind + 1)]
+        lg: [...this.state.layouts.lg.slice(0, ind), ...this.state.layouts.lg.slice(ind + 1)]
       }
     } as AppState);
   }
 
   generateDOM = () => {
     return this.state.layouts.lg.map((l: LayoutItem, i) => {
-      const keyStr = uuid();
       return (
-        <div key={keyStr} className={l.static ? "static" : ""}>
+        <div key={l.i} className={l.static ? "static" : ""}>
           <div className="dragHandle">
             <span
               className="removeBtn"
@@ -84,6 +83,7 @@ class App extends React.Component<AppProps, AppState> {
             >x</span>
           </div>
           <div className="cellContent">
+            <div style={{ marginTop: "20px", textAlign: 'center' }}>{l.i}</div>
           </div>
         </div>
       );
@@ -132,13 +132,13 @@ class App extends React.Component<AppProps, AppState> {
 export default App;
 
 function generateLayout(): LayoutItem[] {
-  return [1, 2, 3, 4, 5].map(function (i, ind) {
+  return [0, 1, 2, 3].map(function (i, ind) {
     var y = Math.ceil(Math.random() * 4) + 1;
     return {
-      x: ind * 2,
-      y: 0,
+      x: (ind * 2) % 12,
+      y: Math.floor(ind / 6),
       w: 2,
-      h: y,
+      h: 2,
       i: uuid(),
       static: false
     };
