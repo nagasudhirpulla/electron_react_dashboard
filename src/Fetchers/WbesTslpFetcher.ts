040d591b16c1018d6a8138cdf2073bf2db0c1dc3
nagasudhirpulla <nagasudhirpulla@gmail.com>
2019-08-26 01:03:29 +0530

    changes

---
+++ src/Fetchers/WbesTslpFetcher.ts
@@ -0,0 +1,35 @@
+import { ITslpDataFetcher } from "./IFetcher";
+import { VarTime } from "../variable_time/VariableTime";
+import { ITslpDataPoint } from "../components/ITimeSeriesLinePlot";
+import { getISGSDcForDates } from "../utils/wbesUtils";
+import { IWbesMeasurement } from "../measurements/WbesMeasurement";
+import { resampleData } from "./PMUTslpFetcher";
+
+export class WbesTslpFetcher implements ITslpDataFetcher {
+    async fetchData(fromVarTime: VarTime, toVarTime: VarTime, meas: IWbesMeasurement): Promise<ITslpDataPoint[]> {
+        const fromTime = VarTime.getDateObj(fromVarTime);
+        const toTime = VarTime.getDateObj(toVarTime);
+        const vals = await getISGSDcForDates(fromTime, toTime, -1, meas.meas_id, meas.schType);
+        if (vals == []) {
+            return [];
+        }
+        // the values returned are of 15 mins freq, so generate timestamps accordingly
+        let samplesStartTime = fromTime;
+        samplesStartTime.setHours(0);
+        samplesStartTime.setMinutes(0);
+        samplesStartTime.setSeconds(0);
+        samplesStartTime.setMilliseconds(0);
+        let resVals: ITslpDataPoint[] = []
+        for (let sampleInd = 0; sampleInd <= vals.length; sampleInd++) {
+            resVals.push(
+                {
+                    timestamp: samplesStartTime.getTime() + 15 * 60 * 1000 * sampleInd,
+                    value: vals[sampleInd]
+                }
+            );
+
+        }
+        resVals = resampleData(resVals, meas.sampling_strategy, meas.periodicity);
+        return resVals;
+    }
+}
---
+++ src/measurements/WbesMeasurement.tsx
@@ -0,0 +1,26 @@
+import { IMeasurement } from "./IMeasurement";
+import { SamplingStrategy, Periodicity } from "./ScadaMeasurement";
+import { TimePeriod } from "../components/ITimeSeriesLinePlot";
+
+export enum SchType {
+    OnBarDc = "OnBarDc",
+    OffBarDc = "OffBarDc",
+    TotalDc = "TotalDc",
+    NetSch = "NetSch"
+}
+
+export interface IWbesMeasurement extends IMeasurement {
+    meas_id: string,
+    schType: SchType,
+    sampling_strategy: SamplingStrategy,
+    periodicity: Periodicity
+};
+
+export class WbesMeasurement implements IWbesMeasurement {
+    schType: SchType = SchType.OnBarDc;
+    static typename: string = "WbesMeasurement"
+    discriminator: string = WbesMeasurement.typename;
+    meas_id: string = "6477e23c-660e-4587-92d2-8e3488bc8262";
+    sampling_strategy: SamplingStrategy = SamplingStrategy.Raw;
+    periodicity: Periodicity = new Periodicity();
+};
